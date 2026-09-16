6bdbac82fadb298aec919f38a8033629b0a5db02
adnoireph <cr27461@gmail.com>
2026-09-16 17:14:48 -0500

    feat(categoria): inicializar arquitectura MVC para Categoria

---
+++ src/main/java/com/agrovalle/agrovalle_connect/controllers/CategoriaController.java
@@ -0,0 +1,6 @@
+package com.agrovalle.agrovalle_connect.controllers;
+import com.agrovalle.agrovalle_connect.services.CategoriaService;
+
+public class CategoriaController {
+    // TODO: Implementar endpoints REST
+}
---
+++ src/main/java/com/agrovalle/agrovalle_connect/services/CategoriaService.java
@@ -0,0 +1,6 @@
+package com.agrovalle.agrovalle_connect.services;
+import com.agrovalle.agrovalle_connect.models.Categoria;
+
+public class CategoriaService {
+    // TODO: Implementar lógica de negocio
+}
